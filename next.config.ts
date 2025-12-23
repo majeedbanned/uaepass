2e79e905ebf69355756d412d692f235bb6d67851
Your Name <you@example.com>
2025-12-23 16:36:18 +0400

    Enable TypeScript and ESLint build error ignoring in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
